67112dd642d39941cd00560f7b7907aadee879b4
asm0dey <pavel.finkelshtein@gmail.com>
2020-06-04 21:09:24 +0300

    Refactoring of SimleApp

--- src/main/kotlin/SimpleApp.kt
+++ src/main/kotlin/SimpleApp.kt
@@ -1,20 +1,13 @@
-/* SimpleApp.kt */
-import org.apache.spark.sql.SparkSession
+@file:JvmName("SimpleApp")
+import org.jetbrains.spark.api.*
 
-object SimpleApp {
-    fun main(args: Array<String>) {
-        val logFile = "/YOUR_SPARK_HOME/README.md" // Change to your Spark's path
-
-        val spark = SparkSession
-            .builder()
-            .master("local[2]")
-            .appName("Simple Application").orCreate
-
-        val logData = spark.read().textFile(logFile).cache()
-
-        val numAs = logData.filter { s: String -> s.contains("a") }.count()
-        val numBs: Long = logData.filter { s: String -> s.contains("b") }.count()
-        println("Lines with a: $numAs, lines with b: $numBs")
-        spark.stop()
+fun main() {
+    val logFile = "/YOUR_SPAwRK_HOME/README.md" // Change to your Spark's path
+    withSpark {
+        spark.read().textFile(logFile).withCached {
+            val numAs = filter { it.contains("a") }.count()
+            val numBs = filter { it.contains("b") }.count()
+            println("Lines with a: $numAs, lines with b: $numBs")
+        }
     }
 }
